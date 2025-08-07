50f66199d945b3eac7ec5c0e0cb30597661eb377
Slavcho <s.trajkovski02@yahoo.com>
2025-08-07 23:26:30 +0200

    PL-5: Implement featured companies section

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -4,17 +4,21 @@ import Navbar from "./navbar/navbar";
 import { MountainIcon } from "lucide-react";
 import Link from "next/link";
 import CtaSection from "../shared/cta-section/cta-section";
+import Image from "next/image";
 
 const Header = () => {
   const t = useTranslations("home.header");
 
   return (
     <header className="bg-blue-500 flex h-20 w-full items-center justify-around px-4 md:px-6">
-      {/* TODO: The actual logo should be placed here */}
       <div className="flex items-center gap-2">
         <Link href="/" className="flex items-center justify-center gap-2">
-          <MountainIcon className="h-6 w-6 text-primary" />
-          <span className="font-bold text-lg text-white">{t("logo")}</span>
+          <Image
+            width={160}
+            height={160}
+            src="/assets/logos/poddrzilokalno.png"
+            alt="Poddrzi Lokalno Logo"
+          />
         </Link>
       </div>
 
