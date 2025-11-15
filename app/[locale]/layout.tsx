c159524c5e733d6711b0ac5acc39e93f1be643f4
Slavcho <s.trajkovski02@yahoo.com>
2025-11-15 16:53:48 +0100

    fix build errors

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -6,22 +6,19 @@ import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { setRequestLocale, getTranslations } from "next-intl/server";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import type { Metadata } from "next";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
 
-export function generateStaticParams() {
-  return routing.locales.map((locale) => ({ locale }));
-}
+export async function generateMetadata(props: {
+  params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+  const { locale } = await props.params;
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { locale: string };
-}) {
-  const t = await getTranslations("metadata");
+  const t = await getTranslations({ locale, namespace: "metadata" });
 
   return {
     title: t("title"),
@@ -29,7 +26,17 @@ export async function generateMetadata({
   };
 }
 
-export default async function RootLayout({ children, params }: any) {
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
+
+export default async function RootLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}) {
   const { locale } = await params;
 
   if (!hasLocale(routing.locales, locale)) notFound();
@@ -39,7 +46,9 @@ export default async function RootLayout({ children, params }: any) {
   return (
     <html lang={locale}>
       <body className={`${montserrat.className} antialiased`}>
-        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+        <NextIntlClientProvider locale={locale}>
+          {children}
+        </NextIntlClientProvider>
         <Analytics />
         <SpeedInsights />
       </body>
